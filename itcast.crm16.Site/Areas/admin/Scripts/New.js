4da2e077632914fdc1fb66cffe9f1470d0e905ac
DevinK <631916104@qq.com>
2016-11-27 21:01:35 +0800

    update:新闻列表

---
+++ itcast.crm16.Site/Areas/admin/Scripts/New.js
@@ -0,0 +1,17 @@
+﻿$(function()
+{
+    //控制新增的弹层
+    var $modal = $('#doc-modal-1');
+ 
+    $('#addNews').on('click', function (e) {
+        $modal.width=420;
+      var $target = $(e.target);
+      if (($target).hasClass('js-modal-open')) {
+        $modal.modal();
+      } else if (($target).hasClass('js-modal-close')) {
+        $modal.modal('close');
+      } else {
+        $modal.modal('toggle');
+      }
+    });
+})
