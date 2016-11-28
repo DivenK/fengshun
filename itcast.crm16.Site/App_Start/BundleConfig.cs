24ee8729f3049bc3db3843fdd43767ad692e888a
DevinK <631916104@qq.com>
2016-11-28 22:09:05 +0800

    update: 添加富文本编辑器

--- itcast.crm16.Site/App_Start/BundleConfig.cs
+++ itcast.crm16.Site/App_Start/BundleConfig.cs
@@ -17,7 +17,11 @@ namespace itcast.crm16.Site
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/Content/ueditor").Include(
 
+            "~/Content/ueditor/ueditor.config.js",
+
+            "~/Content/ueditor/ueditor.all.min.js"));
             // 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
