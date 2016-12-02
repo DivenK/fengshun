faf17953c8c7a79bbd3b44146a0c10762f8fa230
Jay <935812471@qq.com>
2016-12-02 21:04:56 +0800

    Merge remote-tracking branch 'origin/master' into jay/manage
    # Conflicts:
    #	.vs/Supercharger/itcast.crm16.Site/codemap.dat
    #	.vs/Supercharger/itcast.crm16.Site/global_history.dat
    #	.vs/itcast.crm16/v14/.suo
    #	itcast.crm16.Site/Areas/admin/Scripts/New.js
    #	itcast.crm16.Site/Areas/admin/Views/New/Index.cshtml
    #	itcast.crm16.Site/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- itcast.crm16.Site/Areas/admin/Scripts/New.js
+++ itcast.crm16.Site/Areas/admin/Scripts/New.js
@@ -5,13 +5,34 @@
  
     $('#addNews').on('click', function (e) {
         $modal.width=420;
-      var $target = $(e.target);
+        var $target = $(e.target);
+        //使用详情看 http://amazeui.org/javascript/modal
       if (($target).hasClass('js-modal-open')) {
+<<<<<<< HEAD
         $modal.modal();//弹起
       } else if (($target).hasClass('js-modal-close')) {
-        $modal.modal('close');//关闭
+        $modal.modal('close');//关闭  
       } else {
         $modal.modal('toggle');
+=======
+          $modal.modal({
+              width: 900,
+              height:800
+          });
+>>>>>>> origin/master
       }
     });
+    $('#Cancel').on('click', function () {
+        $modal.modal('close');
+    });
+    //分页js加载
+    $('#pageDemo').page({
+            pages:10,
+            first: "首页", //设置false则不显示，默认为false  
+            last: "尾页", //设置false则不显示，默认为false      
+            prev: '<', //若不显示，设置false即可，默认为上一页
+            next: '>', //若不显示，设置false即可，默认为下一页
+            groups: 3, //连续显示分页数
+            jump: function () { }//这里就是去异步请求方法
+        });
 })
