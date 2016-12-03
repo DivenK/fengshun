dd5b86b1fe3e23dd0c8cad11a9c09a3e3269565e
Jay <935812471@qq.com>
2016-12-03 10:16:23 +0800

    manage.js

---
+++ itcast.crm16.Site/Areas/admin/Scripts/manage.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    //控制新增的弹层
+    var $modal = $('#doc-modal-1');//弹窗div  
+ 
+    $('#btnAddManage').on('click', function (e) {        
+        $modal.modal({ closeViaDimmer: 0, width: 800, height: 900});//弹起
+    });
+
+})
