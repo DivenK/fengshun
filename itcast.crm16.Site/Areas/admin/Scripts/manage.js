e869a3df2369754128ebaf879e5b3a3d66f34bce
Jay <935812471@qq.com>
2016-12-05 20:52:02 +0800

    manage页面和js

--- itcast.crm16.Site/Areas/admin/Scripts/manage.js
+++ itcast.crm16.Site/Areas/admin/Scripts/manage.js
@@ -1,9 +1,154 @@
-﻿$(function () {
+﻿var addFlag = 0;
+var editId = 0;
+var pageIndex = 1;
+var pageSize = 15;
+
+
+$(function () {
     //控制新增的弹层
     var $modal = $('#doc-modal-1');//弹窗div  
- 
-    $('#btnAddManage').on('click', function (e) {        
-        $modal.modal({ closeViaDimmer: 0, width: 800, height: 900});//弹起
+
+    $('#btnAddManage').on('click', function (e) {
+        //修改新增标识
+        addFlag = 0;
+        $modal.modal({ closeViaDimmer: 0, width: 800, height: 800 });//弹起
     });
 
+    //  GetContentByPage(1,15,"");
+
+    /******************************
+     1.0 修改或者新增保存点击事件
+    ******************************/
+    $("#formSubmit").unbind("click").bind("click", function () {
+        var title = $("#doc-title").val();
+        if (title == "") {
+            $("#doc-title").focus();
+            return;
+        }
+        var contentModel = {
+            "flag": addFlag,
+            "id": 0,
+            "title": title,
+            "conter": editor.getContent(),
+            "author": $("#doc-author").val()
+        }
+        $.ajax({
+            url: "/admin/manage/change",
+            data: contentModel,
+            type: "post",
+            dataType: 'json',
+            success: function (retContent) {
+                if (retContent.status == 0) {
+                    bfeMsgBox.success("", retContent.msg);
+                    window.location.reload();
+                }
+                else {
+                    bfeMsgBox.error("", retContent.msg);
+                }
+            },
+            error: function (er) {
+                bfeMsgBox.error("", er);
+            }
+        });
+    })
+
+
+    /****************************************
+    2.0 富文本编辑器取消
+    ****************************************/
+    $("#Cancel").unbind("click").bind("click", function () {
+        $modal.modal('close');
+    })
+
+
+    /****************************************
+    3.0 搜索查询数据
+    ****************************************/
+    $("#btnSearch").unbind("click").bind("click", function () {
+        var searchContent = $("#txtSearchContent").val();
+        //if (searchContent == "") {
+        //    bfeMsgBox.error("", "请输入查询条件");
+        //    $("#txtSearchContent").focus();
+        //    return;
+        //}
+        GetContentByPage(pageIndex, pageSize, searchContent)
+    })
 })
+
+/***********************
+获取数据
+***********************/
+function GetContentByPage(pageIndex, pageSize, searchMsg) {
+    var content = {
+        "pageIndex": pageIndex,
+        "PageSize": pageSize,
+        "searchContent": searchMsg
+    };
+    $.post("/Admin/Manage/GetManageData", content, function (retContent) {
+        if (retContent.status == 0) {
+            var totalPage = retContent.datas.TotalPage;
+            var dataContent = retContent.datas.Content;
+
+            $("#new-pageCount").text(totalPage);
+            //TODO：将totalPage赋值给分页插件
+
+            //将数据展示到页面
+            var showHtml = "";
+            for (var i = 0; i < dataContent.length; i++) {
+                showHtml += '<tr>';
+                showHtml += '<td>' + (i + 1) + '</td>';
+                showHtml += '<td>' + dataContent[i].Title + '</td>';
+                showHtml += '<td class="am-hide-sm-only" data-id="1"><div _switch="" class="am-switch am-round am-switch-success  newDisplay am-active"><div class="am-switch-handle"><input type="checkbox" class="" checked=""></div></div></td>';
+                showHtml += '<td>' + dataContent[i].Author + '</td>';
+                showHtml += '<td class="am-hide-sm-only">' + dataContent[i].Creator + '</td>';
+                showHtml += '<td class="am-hide-sm-only">' + dataContent[i].CreateTime + '</td>';
+                showHtml += '<td><div class="am-btn-toolbar"><div class="am-btn-group am-btn-group-xs">';
+                showHtml += '<a class="am-btn am-btn-default am-btn-xs am-text-secondary" id="btnEdit" data-id="' + dataContent[i].id + '"><span class="am-icon-pencil-square-o"></span> 编辑</a>';
+                showHtml += '<a class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" onclick="Del(@item.id)"><span class="am-icon-trash-o"></span> 删除</a>';
+                showHtml += '</div></div></td></tr>';
+                //-----------------
+                //showHtml += '<tr>';
+                //showHtml +='<td>'+ (i + 1) + '</td>';
+                //showHtml +='<td>' + dataContent[i].Title +'</td>';
+                //showHtml +='<td class="am-hide-sm-only" data-id="'+dataContent[i].id+'"><div _switch="" class="am-switch am-round am-switch-success  newDisplay am-active"><div class="am-switch-handle"><input type="checkbox" class="" checked=""></div></div></td>';
+                //showHtml +='<td>' + dataContent[i].Author + '</td>';
+                //showHtml += '<td class="am-hide-sm-only">' + dataContent[i].Creator + '</td>';
+                //showHtml += '<td class="am-hide-sm-only">' + dataContent[i].CreateTime + '</td>';
+                //showHtml += '<td><div class="am-btn-toolbar"><div class="am-btn-group am-btn-group-xs">';
+                //showHtml += '<a class="am-btn am-btn-default am-btn-xs am-text-secondary" id="btnEdit"><span class="am-icon-pencil-square-o"></span> 编辑</a>';
+                //showHtml += '<a class="am-btn am-btn-default am-btn-xs am-text-danger am-hide-sm-only" onclick="Del(1)"><span class="am-icon-trash-o"></span> 删除</a>';
+                //showHtml += '</div></div></td></tr>';
+            }
+            $("#showTb").html(showHtml);
+        }
+    }, "json")
+}
+
+
+/*******************************
+删除
+*******************************/
+function Del(mId) {
+    if (mId == null) {
+        bfeMsgBox.error("", "数据异常，请刷新重试");
+        return;
+    }
+    $.ajax({
+        url: "/admin/manage/Del",
+        data: { "id": mId },
+        type: "post",
+        dataType: "json",
+        success: function (e) {
+            if (e.status == 0) {
+                window.location.reload();
+            }
+            else {
+                bfeMsgBox.error("", e.msg);
+            }
+        },
+        error: function (er) {
+            bfeMsgBox.error("", er);
+        }
+    })
+}
+
